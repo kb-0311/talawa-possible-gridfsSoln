3e8adec913f327dfccf3470ad388915710552833
Kanishka <kanishka.bansode@gmail.com>
2023-02-04 13:17:38 +0530

    cleaning up code

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const { rejects } = require('assert');
-const assert = require('assert');
 const fs = require('fs');
 const mongodb = require('mongodb');
 const { default: mongoose } = require('mongoose');
@@ -14,34 +12,34 @@ const dbName = 'test';
 mongoose.connect('mongodb://localhost:27017/test?retryWrites=true&w=majority').then((data)=>{
     console.log(`mongoDB connected succesfully on server ${data.connection.host}`);
 })
+
+
+// The main config needed
 const client = new mongoose.mongo.MongoClient(uri);
-
-
-// The main controller of the file 
-
-  const db = client.db(dbName);
-  let bucket = new mongodb.GridFSBucket(db);
+const db = client.db(dbName);
+let bucket = new mongodb.GridFSBucket(db);
 
 
 
 //To upload the image to the bucket
 const upload = async ()=>{
+
   await pipeline(      
       fs.createReadStream('sample.png'),
       bucket.openUploadStream('sample.png')
+    
 )}
 
 
 // To retrieve file from database and write the contentes in FS as temporary cache
 const get = async() =>{
 
-        await pipeline(
-          bucket.openDownloadStreamByName('sample.png'), 
-          fs.createWriteStream('./sample2.png')
-          .on('finish' , ()=>{
-            // process.exit(0)
-          })
-        )
+    await pipeline(
+      bucket.openDownloadStreamByName('sample.png'), 
+      fs.createWriteStream('./sample2.png')
+        .on('finish' , ()=>{
+      })
+    )
 
 }
 
